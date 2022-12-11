6b99d1b346fbc567538b65b7bc1199fe6057b8be
bnriiitb <nagaraju.iiit@gmail.com>
2022-12-11 01:49:34 -0500

    cleand up

--- notebooks/download_data.sh
+++
@@ -1,7 +0,0 @@
-# wget https://storage.googleapis.com/test_public_bucket/labelled/Telugu_Chai_Bisket_Stories_16-08-2021_14-12.zip
-# wget https://storage.googleapis.com/test_public_bucket/labelled/Telugu_Chai_Bisket_Stories_16-08-2021_14-14.zip
-# wget https://storage.googleapis.com/test_public_bucket/labelled/Telugu_Chai_Bisket_Stories_16-08-2021_14-17.zip
-# unzip Telugu_Chai_Bisket_Stories_16-08-2021_14-12.zip
-# unzip Telugu_Chai_Bisket_Stories_16-08-2021_14-14.zip
-# unzip Telugu_Chai_Bisket_Stories_16-08-2021_14-17.zip
-rm -r *.zip
--- setup.bash
+++
@@ -1,3 +0,0 @@
-# conda activate telugu_asr
-# jupyter lab
-netstat -ano | findstr 8080
