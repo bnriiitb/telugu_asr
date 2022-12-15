520af7879fe53da359ba364479b8ff518ed744b8
bnriiitb <nagaraju.iiit@gmail.com>
2022-12-15 15:47:56 -0500

    updated training code for new dataset

--- notebooks/train.py
+++ notebooks/train.py
@@ -5,24 +5,24 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-datasets_urls = pd.read_csv("../data/asr_dataset_info.csv")
+# datasets_urls = pd.read_csv("../data/asr_dataset_info.csv")
 
-dataset_file_names = [link.replace("https://storage.googleapis.com/test_public_bucket/labelled/Telugu_","").replace(".zip","") 
-                      for link in datasets_urls.corpus_cownload_link ]
-print(dataset_file_names)
-
-
-DOWNLOAD_URLS = datasets_urls[datasets_urls.source.isin(['Zee_Telugu','idream_telugu_movies',
-                                         'telangana_sahitya_akademi',
-                                         'dd_saptagiri',
-                                        'chai_bisket_stories'])].corpus_cownload_link.unique().tolist()
-print(DOWNLOAD_URLS[:2])
+# dataset_file_names = [link.replace("https://storage.googleapis.com/test_public_bucket/labelled/Telugu_","").replace(".zip","") 
+#                       for link in datasets_urls.corpus_cownload_link ]
+# print(dataset_file_names)
 
 
-dataset_file_names = [link.replace("https://storage.googleapis.com/test_public_bucket/labelled/Telugu_","").replace(".zip","") 
-                      for link in DOWNLOAD_URLS]
-print("##### Dataset file names #####")
-print(dataset_file_names)
+# DOWNLOAD_URLS = datasets_urls[datasets_urls.source.isin(['Zee_Telugu','idream_telugu_movies',
+#                                          'telangana_sahitya_akademi',
+#                                          'dd_saptagiri',
+#                                         'chai_bisket_stories'])].corpus_cownload_link.unique().tolist()
+# print(DOWNLOAD_URLS[:2])
+
+
+# dataset_file_names = [link.replace("https://storage.googleapis.com/test_public_bucket/labelled/Telugu_","").replace(".zip","") 
+#                       for link in DOWNLOAD_URLS]
+# print("##### Dataset file names #####")
+# print(dataset_file_names)
 
 import os
 from IPython import display
@@ -30,32 +30,57 @@ import datasets
 from datasets.info import DatasetInfosDict
 from datasets import load_dataset,concatenate_datasets, DatasetDict, Dataset, Audio
 
-def get_dataset(file_name):
+# def get_dataset(file_name):
+#     ds_base_path = os.getcwd()+"/training_data/"+file_name
+#     print(f'ds_base_path --> {ds_base_path}')
+#     ds = load_dataset('json', data_files=ds_base_path+"/data.json")
+#     ds = ds.map(lambda x: {'audioFilename':ds_base_path+"/"+x["audioFilename"]})
+#     return ds
+
+# ds = get_dataset(dataset_file_names[0])['train']
+# print(ds)
+
+# print(ds)
+# for fn in tqdm(dataset_file_names[1:]):
+#     temp_ds = get_dataset(fn)['train']
+#     ds = concatenate_datasets([ds,temp_ds])
+#     print(ds)
+
+
+def get_dataset_from_json(file_name):
     ds_base_path = os.getcwd()+"/training_data/"+file_name
-    print(f'ds_base_path --> {ds_base_path}')
     ds = load_dataset('json', data_files=ds_base_path+"/data.json")
     ds = ds.map(lambda x: {'audioFilename':ds_base_path+"/"+x["audioFilename"]})
-    return ds
+    ds = ds.remove_columns(['collectionSource', 'snr', 'duration', 'gender'])
+    return ds['train']
 
-ds = get_dataset(dataset_file_names[0])['train']
-print(ds)
+def get_dataset_from_csv(file_name):
+    ds_base_path = os.getcwd()+"/training_data/"+file_name
+    ds = load_dataset('csv', data_files=ds_base_path+"/metadata.csv")
+    ds = ds.map(lambda x: {'file_name':ds_base_path+"/"+x["file_name"]})
+    ds = ds.rename_column("file_name", "audioFilename")
+    ds = ds.rename_column("transcription", "text")
+    return ds['train']
 
+csv_files = ["noisy_test_unknown","noisy_test_known","clean_test_known","clean_test_unknown","clean_valid","te_in_female","te_in_male"]
+ds = get_dataset_from_json("Telangana_Sahitya_Akademi_16-08-2021_14-40/")
 print(ds)
-for fn in tqdm(dataset_file_names[1:]):
-    temp_ds = get_dataset(fn)['train']
+for fn in csv_files:
+    temp_ds = get_dataset_from_csv(fn)
     ds = concatenate_datasets([ds,temp_ds])
     print(ds)
 
 ds = ds.cast_column("audioFilename",Audio(sampling_rate=16000))
 ds = ds.rename_column("audioFilename", "audio")
 
-print({f"Training on {(np.sum(ds['duration'])/60)/60} hours of data"})
+# print({f"Training on {(np.sum(ds['duration'])/60)/60} hours of data"})
 
 
 train_test_dataset = ds.train_test_split(test_size=0.15)
-train_test_dataset = train_test_dataset.remove_columns(['collectionSource', 'snr', 'duration', 'gender'])
+# train_test_dataset = train_test_dataset.remove_columns(['collectionSource', 'snr', 'duration', 'gender'])
 print(train_test_dataset)
 
+print("####### Loaded all the datasets, ready for tuning#########")
 
 from transformers import WhisperFeatureExtractor
 from transformers import WhisperTokenizer
@@ -76,13 +101,17 @@ def prepare_dataset(batch):
     # batch["labels"] = tokenizer(batch["sentence"]).input_ids
     return batch
 
-train_test_dataset = train_test_dataset.map(prepare_dataset, remove_columns=train_test_dataset.column_names["train"], num_proc=32)
+print("####### Preparing dataset started ########")
+train_test_dataset = train_test_dataset.map(prepare_dataset, remove_columns=train_test_dataset.column_names["train"], num_proc=20)
+print("####### Preparing dataset completed ########")
+
 
 print("####### Define a Data Collator ########")
 import torch
-
 from dataclasses import dataclass
 from typing import Any, Dict, List, Union
+
+torch.cuda.empty_cache()
 
 @dataclass
 class DataCollatorSpeechSeq2SeqWithPadding:
@@ -149,8 +178,8 @@ print("####### Define the Training Configuration #########")
 from transformers import Seq2SeqTrainingArguments
 
 training_args = Seq2SeqTrainingArguments(
-    output_dir="./whisper-small-te",  # change to a repo name of your choice
-    per_device_train_batch_size=16,
+    output_dir="./whisper-small-te-14k",  # change to a repo name of your choice
+    per_device_train_batch_size=8,
     gradient_accumulation_steps=1,  # increase by 2x for every 2x decrease in batch size
     learning_rate=1e-5,
     warmup_steps=500,
